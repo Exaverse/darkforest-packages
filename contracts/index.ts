3ec2e93d583e60d31b21bb7b6bf6aea5a87afbce
Omar Mezenner <omar@kiosk.dev>
2022-03-16 19:46:37 -0700

    ?

--- contracts/index.ts
+++ contracts/index.ts
@@ -46,8 +46,8 @@ export const START_BLOCK = 0;
 /**
  * The address for the DarkForest contract.
  */
-export const CONTRACT_ADDRESS = '0xBF26Bc9DB43EdE038731b78784660852E3495413';
+export const CONTRACT_ADDRESS = '0x500cf53555c09948f4345594F9523E7B444cD67E';
 /**
  * The address for the initalizer contract. Useful for lobbies.
  */
-export const INIT_ADDRESS = '0x8A0ABbca169B3BE30539b5D8Bbe6ed3b1FC7C046';
+export const INIT_ADDRESS = '0xc2EEd1AdAAe36B640f64c06FeF8Cd32F7473afAA';
